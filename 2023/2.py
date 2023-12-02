19a67c7e45f9dc4ed7d35705ff4a45821dec0505
Christian Reinbacher <reinbc@amazon.com>
2023-12-02 10:59:40 +0100

    formatting

--- 2023/2.py
+++ 2023/2.py
@@ -3,9 +3,9 @@ from typing import List, Dict
 from aoc import day, get_input
 
 
-def count_bags(input: str) -> Dict[int, str]:
+def count_bags(input: str) -> Dict[str, int]:
     result = {"red": 0, "blue": 0, "green": 0}
-    game, contents = input.split(':')
+    _, contents = input.split(':')
     for draw in contents.split(';'):
         for count, color in [x.split() for x in draw.split(',')]:
             result[color] = max(result[color], int(count))
@@ -42,8 +42,10 @@ def puzzle_input():
     return [
         'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
         'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 '
+        'red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 '
+        'red',
         'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
     ]
 
