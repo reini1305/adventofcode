82420e015a51565bf6f4ea80012ca543b9744835
Christian Reinbacher <reinbc@amazon.com>
2023-12-05 09:46:56 +0100

    Day 5 only works in pypy

--- 2023/5.py
+++ 2023/5.py
@@ -1,5 +1,6 @@
 import pytest
 from typing import List, Tuple
+from tqdm import tqdm
 from aoc import day, get_input
 
 
@@ -8,21 +9,20 @@ def get_mappings(input: List[str]) -> List[List[Tuple[int, int, int]]]:
     curr_mapping: List[Tuple[int, int, int]] = []
     for line in input:
         if line.startswith('seeds:'):
-            # _, seed_str = line.split(': ')
-            # curr_dict = {int(x): int(x) for x in seed_str.split()}
             continue
         elif line == '':
             if curr_mapping:
                 mappings.append(curr_mapping)
             curr_mapping = []
         elif line[0].isdigit():
-            source_start, dest_start, length = line.split()
+            dest_start, source_start, length = line.split()
             ss = int(source_start)
             ds = int(dest_start)
             le = int(length)
-            curr_mapping.append((ss, ds, le))
+            curr_mapping.append((ds, ss, le))
         else:
             continue  # Description
+    mappings.append(curr_mapping)
     return mappings
 
 
@@ -35,17 +35,36 @@ def get_seeds(input: List[str]) -> List[int]:
 
 
 def traverse_mappings(start: int, mappings: List[List[Tuple[int, int, int]]]) -> int:
-    return 0
+    source = start
+    dest = source
+    for mapping in mappings:
+        for (ds, ss, le) in mapping:
+            if source >= ss and source < ss + le:
+                dest = ds + (source - ss)
+                break
+        else:
+            dest = source
+        source = dest
+    return dest
 
 
 def part1(input: List[str]) -> int:
-    result = 0
+    result = 1000000000
+    seeds = get_seeds(input)
+    mappings = get_mappings(input)
+    for seed in seeds:
+        result = min(result, traverse_mappings(seed, mappings))
     print(f'Day {day()}, Part 1: {result}')
     return result
 
 
 def part2(input: List[str]) -> int:
-    result = 0
+    result = 1000000000
+    seeds = get_seeds(input)
+    mappings = get_mappings(input)
+    for s in tqdm(range(0, len(seeds), 2)):
+        for seed in range(seeds[s], seeds[s] + seeds[s + 1]):
+            result = min(result, traverse_mappings(seed, mappings))
     print(f'Day {day()}, Part 2: {result}')
     return result
 
@@ -100,7 +119,11 @@ def test_day5_part1(puzzle_input):
     assert seeds == [79, 14, 55, 13]
     mappings = get_mappings(puzzle_input)
     assert traverse_mappings(seeds[0], mappings) == 82
+    assert traverse_mappings(seeds[1], mappings) == 43
+    assert traverse_mappings(seeds[2], mappings) == 86
+    assert traverse_mappings(seeds[3], mappings) == 35
+    assert part1(puzzle_input) == 35
 
 
 def test_day5_part2(puzzle_input):
-    assert 1
+    assert part2(puzzle_input) == 46
