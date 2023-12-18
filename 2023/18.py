bde00a7a2d70f0efe54f42e93638f7dddbf01c4c
Christian Reinbacher <reinbc@amazon.com>
2023-12-18 11:45:27 +0100

    Speeeed

--- 2023/18.py
+++ 2023/18.py
@@ -1,6 +1,6 @@
 import pytest
 from typing import List, Tuple
-from aoc import day, get_input, tuple_add
+from aoc import day, get_input, tuple_add, tuple_mul
 
 
 def parse_input(input: List[str]) -> List[Tuple[str, int, str]]:
@@ -11,37 +11,37 @@ def parse_input(input: List[str]) -> List[Tuple[str, int, str]]:
     return output
 
 
-def get_path(instructions: List[Tuple[str, int, str]], part2=False) -> List[Tuple[int, int]]:
+def get_path(instructions: List[Tuple[str, int, str]], part2=False) -> Tuple[List[Tuple[int, int]], int]:
     path = []
     directions = {'L': (-1, 0), 'R': (1, 0), 'U': (0, -1), 'D': (0, 1)}
     dir_mappings = 'RDLU'
     current = (0, 0)
+    total_count = 0
     for direction, amount, color in instructions:
         count = amount if not part2 else int(color[:-1], 16)
         dir = direction if not part2 else dir_mappings[int(color[-1])]
-        for _ in range(count):
-            current = tuple_add(current, directions[dir])
-            path.append(current)
-    return path
+        # for _ in range(count):
+        current = tuple_add(current, tuple_mul(directions[dir], count))
+        path.append(current)
+        total_count += count
+    return path, total_count
 
 
-def count_inside(path: List[Tuple[int, int]]) -> int:
+def count_inside(path: List[Tuple[int, int]], length) -> int:
     A = sum(a[0] * b[1] - b[0] * a[1] for a, b in zip(path, path[1:] + [path[0]]))
-    return A // 2 + len(path) // 2 + 1
+    return A // 2 + length // 2 + 1
 
 
 def part1(input: List[str]) -> int:
     instruction = parse_input(input)
-    path = get_path(instruction)
-    result = count_inside(path)
+    result = count_inside(*get_path(instruction))
     print(f'Day {day()}, Part 1: {result}')
     return result
 
 
 def part2(input: List[str]) -> int:
     instruction = parse_input(input)
-    path = get_path(instruction, True)
-    result = count_inside(path)
+    result = count_inside(*get_path(instruction, True))
     print(f'Day {day()}, Part 2: {result}')
     return result
 
@@ -74,8 +74,8 @@ def puzzle_input():
 
 def test_day18_part1(puzzle_input):
     instruction = parse_input(puzzle_input)
-    path = get_path(instruction)
-    assert count_inside(path) == 62
+    path, length = get_path(instruction)
+    assert count_inside(path, length) == 62
 
 
 def test_day18_part2(puzzle_input):
--- 2023/aoc.py
+++ 2023/aoc.py
@@ -21,3 +21,7 @@ def get_num_input() -> List[int]:
 
 def tuple_add(a: Tuple[int, int], b: Tuple[int, int]) -> Tuple[int, int]:
     return (a[0] + b[0], a[1] + b[1])
+
+
+def tuple_mul(a: Tuple[int, int], b: int) -> Tuple[int, int]:
+    return (a[0] * b, a[1] * b)
